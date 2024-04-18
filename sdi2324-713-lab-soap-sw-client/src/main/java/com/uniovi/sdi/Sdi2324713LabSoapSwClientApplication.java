dbe0a0c65f78884fc77c706e67a943c079071ad6
David <uo289792@uniovi.es>
2024-04-18 10:19:18 +0200

    SDI-2324-713-SW-11.1-Servicio SOAP Cliente.

---
+++ sdi2324-713-lab-soap-sw-client/src/main/java/com/uniovi/sdi/Sdi2324713LabSoapSwClientApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324713LabSoapSwClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2324713LabSoapSwClientApplication.class, args);
+	}
+
+}
